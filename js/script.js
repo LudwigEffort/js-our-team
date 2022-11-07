6637286cd43ad4e4bee8334b79f70b9772fd3026
LudwigVonEffort <dr.feynman65@gmail.com>
2022-11-07 17:56:56 +0100

    add objects and array

--- js/script.js
+++ js/script.js
@@ -15,3 +15,47 @@ Consigli del giorno:
 Ragioniamo come sempre a step.
 Prima la logica in italiano e poi traduciamo in codice.
 */
+
+const person1 = {
+    'name': 'Wayne Barnett',
+    'job': 'Founder & CEO',
+    'img': 'wayne-barnett-founder-ceo.jpg'
+};
+
+const person2 = {
+    'name': 'Angela Caroll',
+    'job': 'Chief Editor',
+    'img': 'angela-caroll-chief-editor.jpg'
+};
+
+const person3 = {
+    'name': 'Walter Gordon',
+    'job': 'Office Manager',
+    'img': 'walter-gordon-office-manager.jpg',
+};
+
+const person4 = {
+    'name': 'Angela Lopez',
+    'job': 'Social Media Manager',
+    'img': 'angela-lopez-social-media-manager.jpg'
+};
+
+const person5 = {
+    'name': 'Scott Estrada',
+    'job': 'Developer',
+    'img': 'scott-estrada-developer.jpg',
+};
+
+const person6 = {
+    'name': 'Barbara Ramos',
+    'job': 'Graphic Designer',
+    'img': 'barbara-ramos-graphic-designer.jpg'
+};
+
+const arrEmployes = [person1, person2, person3, person4, person5, person6];
+
+// for (let key in person1) {
+//     console.log(person1[key]);
+// }
+
+console.log(arrEmployes);
